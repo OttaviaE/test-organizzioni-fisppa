26c108a8bb817f79171800331952973911c24e66
Mar-Cald <margherita.calderan@gmail.com>
2026-02-26 12:03:41 +0100

    uodate website and slide

---
+++ slides/01-IntroR/esercizi/vettori-fattori.R
@@ -0,0 +1,26 @@
+# Esercizi vettori e fattori
+
+# 1 Crea il vettore num contenente i conteggi giornalieri di 
+# libri venduti (da 15 a 27). 
+
+
+# 2 Estrai il quarto e il l'ultimo elemento di num
+# ?length
+
+# 3 Calcola la radice quadrata del numero di libri in num 
+
+# 4 Crea l'oggetto s il cui valore è dato dalla somma del numero di libri
+
+
+# 5 Assegna il valore s agli elementi di num che hanno 
+# minuti compresi tra 16–20
+
+
+# 6 Crea un fattore colori con i livelli "rosso", "verde", "blu", 
+# ciascuno ripetuto 3 volte. ?rep
+
+# 7 Visualizza i livelli del fattore. 
+
+# 8 Rinomina i livelli in "col_rosso", "col_verde", "col_blu".
+
+
