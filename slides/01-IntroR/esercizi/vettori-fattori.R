af0a158ed99af199d79f965332d6f580fe980998
Mar-Cald <margherita.calderan@gmail.com>
2026-03-01 18:57:01 +0100

    up slide R

--- slides/01-IntroR/esercizi/vettori-fattori.R
+++ slides/01-IntroR/esercizi/vettori-fattori.R
@@ -1,26 +1,33 @@
 # Esercizi vettori e fattori
 
-# 1 Crea il vettore num contenente i conteggi giornalieri di 
-# libri venduti (da 15 a 27). 
+# 0.1) crea l'oggetto x di valore 10
+# 0.2) crea l'oggetto y di valore 20
+# 0.3) valuta se x è maggiore di 20
+
+# 0.4) crea l'oggetto nome e assegnagli il tuo nome
+# 0.5) crea l'oggetto cognome e assegnali il tuo cognome
+# 0.6) valuta se nome è minore o uguale a cognome 
 
 
-# 2 Estrai il quarto e il l'ultimo elemento di num
+# 1) Crea il vettore num contenente i conteggi giornalieri di 
+# libri venduti (da 15 a 27). (potresti utilizzare la funzione seq(), 
+# prova a vedere come funziona lanciando il comando ?seq)
+
+# 2) Estrai il quarto e il l'ultimo elemento di num
 # ?length
 
-# 3 Calcola la radice quadrata del numero di libri in num 
+# 3) Calcola la radice quadrata del numero di libri in num 
 
-# 4 Crea l'oggetto s il cui valore è dato dalla somma del numero di libri
+# 4) Crea l'oggetto s il cui valore è dato dalla somma del numero di libri
 
-
-# 5 Assegna il valore s agli elementi di num che hanno 
+# 5) Assegna il valore s agli elementi di num che hanno 
 # minuti compresi tra 16–20
 
-
-# 6 Crea un fattore colori con i livelli "rosso", "verde", "blu", 
+# 6) Crea un fattore colori con i livelli "rosso", "verde", "blu", 
 # ciascuno ripetuto 3 volte. ?rep
 
-# 7 Visualizza i livelli del fattore. 
+# 7) Visualizza i livelli del fattore. 
 
-# 8 Rinomina i livelli in "col_rosso", "col_verde", "col_blu".
+# 8) Rinomina i livelli in "col_rosso", "col_verde", "col_blu".
 
 
---
+++ slides/pkg.R
@@ -0,0 +1,5 @@
+# Pacchetti utili da installare, far andare il comando una volta soltanto
+install.packages("dplyr")
+install.packages("readr")
+install.packages("psych")
+install.packages("ggplot2")
